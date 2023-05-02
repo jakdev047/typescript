3e6bdda3d3cf4086b78795f41d824e2c5a7ff316
jakdev047 <jakfreelancer047@gmail.com>
2023-05-02 20:11:30 +0600

    Array and Tuples in typescrip

--- basicTypescript/src/index.ts
+++ basicTypescript/src/index.ts
@@ -1,3 +1,15 @@
+/*
+  Primitive Data type
+    string number boolean null undefined
+*/
 const topics: string = "Welcome Typescript !!!";
+const numberOfstudents: number = 50;
+const isKnowJavascript: boolean = true;
 
-console.log(topics);
+console.log(topics, numberOfstudents, isKnowJavascript);
+
+/*
+  Array and Tuples in typescript
+*/
+const studentName: string[] = ["Jubayer", "Sabith", "Hasan"];
+const singleUser: [string, number] = ["Jubayer", 123456];
