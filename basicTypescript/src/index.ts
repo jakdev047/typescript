19d040f6a786478c854307478d20f90a4c1feff5
jakdev047 <jakfreelancer047@gmail.com>
2023-05-02 23:37:59 +0600

    Null, Unknown and Never Types

--- basicTypescript/src/index.ts
+++ basicTypescript/src/index.ts
@@ -162,3 +162,35 @@ const nextLevelDeveloper: NextLevelDeveloper = {
   leadershipExperience: 2,
   level: Level?.senior,
 };
+
+/*
+  Null, Unknown and Never Types
+
+    strictNullChecks
+*/
+
+const getMyCarSpeed = (speed: unknown) => {
+  if (typeof speed === "number") {
+    const convertedSpeed = (speed * 1000) | 3600;
+
+    console.log(`My speed is ${convertedSpeed}`);
+  } else if (typeof speed === "string") {
+    const [value, unit] = speed.split(" ");
+
+    const convertedSpeed = (parseFloat(value) * 1000) | 3600;
+
+    console.log(`My speed is ${convertedSpeed}`);
+  } else {
+    console.log("There is wrong type");
+  }
+};
+
+getMyCarSpeed(10);
+getMyCarSpeed("10 kmh^-1");
+getMyCarSpeed(true);
+
+function throwError(message: string): never {
+  throw new Error(message);
+}
+
+// throwError("Error has happened");
