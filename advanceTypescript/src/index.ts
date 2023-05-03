4ef56e6538e90462d485110548b637af374c3368
jakdev047 <jakfreelancer047@gmail.com>
2023-05-03 20:22:28 +0600

    Interface, Type vs Interface

--- advanceTypescript/src/index.ts
+++ advanceTypescript/src/index.ts
@@ -24,3 +24,47 @@ function kgToGram(param: string | number): string | number | undefined {
 
 const resultToBeString = kgToGram("1000") as string; // string type return
 const resultToBeNumber = <number>kgToGram(1000); // string type return
+
+/*
+  Interface, Type vs Interface
+*/
+
+type User = {
+  name: string;
+  age: number;
+};
+
+interface IUser {
+  name: string;
+  age: number;
+}
+
+interface ExtendUser extends IUser {
+  role: string;
+}
+
+const userWithTypeAlias: User = {
+  name: "Type Alias",
+  age: 100,
+};
+
+const userWithInterface: IUser = {
+  name: "Interface",
+  age: 100,
+};
+
+type addTypeNumbers = (num1: number, num2: number) => number;
+
+const addNumbers: addTypeNumbers = (num1, num2) => num1 + num2;
+
+interface IAddNumbers {
+  (num1: number, num2: number): number;
+}
+
+type rollTypeNumbers = number[];
+
+interface IRollNumbers {
+  [index: number]: number;
+}
+
+const rollNumbers: rollTypeNumbers = [1, 2, 3, 4];
