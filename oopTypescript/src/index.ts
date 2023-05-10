0abc0ba370f5a068ce0535c5428a3bdeff11ac19
jakdev047 <jakfreelancer047@gmail.com>
2023-05-10 07:51:38 +0600

    inheritance

--- oopTypescript/src/index.ts
+++ oopTypescript/src/index.ts
@@ -25,3 +25,45 @@ class Animal {
 const dog = new Animal("German Shepard", "dog", "ghew ghew");
 
 dog.makeSound(); // This German Shepard says ghew ghew
+
+/*
+  Inheritence
+*/
+
+class Person {
+  // constructor
+  constructor(
+    public name: string,
+    public age: string,
+    public address: string
+  ) {}
+
+  // method
+  makeSleep(hours: number): string {
+    return `This ${this.name} will sleep for ${hours}`;
+  }
+}
+
+class Student extends Person {
+  // constructor
+  constructor(public name: string, public age: string, public address: string) {
+    super(name, age, address);
+  }
+}
+
+class Teacher extends Person {
+  // constructor
+  constructor(
+    public name: string,
+    public age: string,
+    public address: string,
+    public designation: string
+  ) {
+    super(name, age, address);
+  }
+
+  // method
+  takeClasses(numOfClass: number): string {
+    return `This ${this.name} will take for ${numOfClass}`;
+  }
+}
